f3001b845925060ee323fbbdf80b4b8f7c473b2e
N1heo <96371464+N1heo@users.noreply.github.com>
2022-03-19 19:02:09 +0600

    Delete eventaiubot directory

--- eventaiubot/keyboard.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
-
-button = KeyboardButton('/event')
-
-event_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button)
