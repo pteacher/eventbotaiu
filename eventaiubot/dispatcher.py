9eb34414d7e57490d50d956e77654f2483ffd8bc
N1heo <96371464+N1heo@users.noreply.github.com>
2022-03-19 19:02:39 +0600

    Add files via upload

---
+++ eventaiubot/dispatcher.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher
+import config
+
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
+# prerequisites
+if not config.BOT_TOKEN:
+    exit("No token provided")
+
+# init
+bot = Bot(token=config.BOT_TOKEN, parse_mode="HTML")
+
+dp = Dispatcher(bot, storage=MemoryStorage())
+dp.middleware.setup(LoggingMiddleware())
+
+
